9e353586ca9038d890741cc5ee7a3a2b630f2cd6
Sai Rukshith <rukshith03@gmail.com>
2024-11-08 10:17:08 +0530

    JS update

--- script.js
+++ script.js
@@ -13,14 +13,4 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(container);
     });
 });
-let currentSlideIndex = 0;
 
-
-function openLightbox(src) {
-    document.getElementById("lightbox-img").src = src;
-    document.getElementById("lightbox").style.display = "flex";
-  }
-
-  function closeLightbox() {
-    document.getElementById("lightbox").style.display = "none";
-  }
